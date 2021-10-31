e2f5d74fa6d09e5751343778522e70342e2b8ed7
tejalwalimbe <93183521+tejalwalimbe@users.noreply.github.com>
2021-11-01 00:20:05 +0530

    Basic Shell Programs

---
+++ div.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+number1=2
+
+number2=2
+
+div=$(($number1/$number2))
+
+echo "The division of 2 numbers is $div"
+
---
+++ sub.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+
+number1=40
+
+number2=30
+
+sub=$(($number1-$number2))
+
+echo " The subtraction of 2 numbers $sub"
